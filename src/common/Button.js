08f48a0b2d355c560cba148e2a3f834187e8106f
Lamia-Abdullah <lamia-albaqami@outlook.com>
2025-07-22 23:17:52 +0300

    fix: button height with input and fixed alignment

--- src/common/Button.js
+++ src/common/Button.js
@@ -2,7 +2,7 @@ export default function Button({ children, onClick }) {
   return (
     <button
       onClick={onClick}
-      className="px-4 py-1 rounded bg-blue-500 hover:bg-blue-600 text-white text-sm font-medium transition"
+className="px-4 py-2 h-9 rounded bg-blue-500 hover:bg-blue-600 text-white text-sm font-medium transition"
     >
       {children}
     </button>
